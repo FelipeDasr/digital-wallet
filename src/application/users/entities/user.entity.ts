515d0457df162ba443294a2bfa64599d6cc63a6a
FelipeDasr <felipe.heiker07@gmail.com>
2025-05-29 21:32:28 -0400

    feat: users entity implementation

--- src/application/users/entities/user.entity.ts
+++ src/application/users/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class UserEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "users" })
+export class UserEntity {
+	@Column()
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
+
+	@Column()
+	fullname: string;
+
+	@Column({ unique: true })
+	email: string;
+
+	@Column()
+	password: string;
+
+	@Column({
+		name: "created_at",
+		default: new Date(),
+	})
+	createdAt: Date;
+
+	@Column({
+		name: "updated_at",
+		default: new Date(),
+	})
+	updatedAt: Date;
+}
