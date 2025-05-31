1b2ed3f6fd624a9c4b3ca57f25bce227dfc3bdbc
FelipeDasr <felipe.heiker07@gmail.com>
2025-05-31 03:41:37 -0400

    refactor: move endpoint

---
+++ src/infra/http/controllers/transfers/transfers.controller.ts
@@ -0,0 +1,38 @@
+import { DepositInWalletUseCase } from "@application/transactions/use-cases/deposit-in-wallet/deposit-in-wallet.use-case";
+
+import { UserEntity } from "@application/users/entities/user.entity";
+
+import { WalletDepositDTO } from "@application/wallets/dtos/wallet-deposit.dto";
+
+import { LoggedInUser } from "@common/decorators/logged-in-user.decorator";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import {
+	ApiBearerAuth,
+	ApiOperation,
+	ApiResponse,
+	ApiTags,
+} from "@nestjs/swagger";
+
+@Controller("transactions")
+@ApiTags("Transactions")
+@ApiBearerAuth()
+export class TransactionsController {
+	constructor(
+		private readonly depositInWalletUseCase: DepositInWalletUseCase,
+	) {}
+
+	@ApiOperation({
+		description: "Deposit an amount into the user's wallet",
+	})
+	@ApiResponse({
+		status: HttpStatus.OK,
+	})
+	@HttpCode(HttpStatus.OK)
+	@Post("deposit")
+	public async depositInWallet(
+		@LoggedInUser() user: UserEntity,
+		@Body() data: WalletDepositDTO,
+	) {
+		await this.depositInWalletUseCase.execute(user, data);
+	}
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,6 +1,6 @@
+import { DepositInWalletUseCase } from "@application/transactions/use-cases/deposit-in-wallet/deposit-in-wallet.use-case";
 import { AuthenticateUserUseCase } from "@application/users/use-cases/authenticate-user/authenticate-user.use-case";
 import { CreateUserUseCase } from "@application/users/use-cases/create-user/create-user.use-case";
-import { DepositInWalletUseCase } from "@application/wallets/use-cases/deposit-in-wallet/deposit-in-wallet.use-case";
 import { FindUserWalletUseCase } from "@application/wallets/use-cases/find-user-wallet/find-user-wallet.use-case";
 
 import { AuthController } from "./controllers/auth/auth.controller";
@@ -10,10 +10,11 @@ import { ProvidersModule } from "@common/providers/providers.module";
 import { AuthModule } from "@infra/auth/auth.module";
 import { DatabaseModule } from "@infra/database/database.module";
 import { Module } from "@nestjs/common";
+import { TransactionsController } from "./controllers/transfers/transfers.controller";
 
 @Module({
 	imports: [DatabaseModule, AuthModule, ProvidersModule],
-	controllers: [AuthController, WalletsController],
+	controllers: [AuthController, WalletsController, TransactionsController],
 	providers: [
 		CreateUserUseCase,
 		AuthenticateUserUseCase,
