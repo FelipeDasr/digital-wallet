e421dc95922b53608619a035f3448fbbb2862a65
FelipeDasr <felipe.heiker07@gmail.com>
2025-06-01 17:31:35 -0400

    fix: migrations

--- src/infra/database/typeorm/migrations/1748813464078-migration.ts
+++ src/infra/database/typeorm/migrations/1748813464078-migration.ts
@@ -1,17 +1,23 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migration1748666515557 implements MigrationInterface {
-	name = "Migration1748666515557";
+export class Migration1748813464078 implements MigrationInterface {
+	name = "Migration1748813464078";
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
-			`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "fullname" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT '"2025-05-31T04:42:02.260Z"', "updated_at" TIMESTAMP NOT NULL DEFAULT '"2025-05-31T04:42:02.260Z"', CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+			`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "fullname" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
 		);
 		await queryRunner.query(
 			`CREATE TABLE "wallets" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "balance" integer NOT NULL DEFAULT '0', "user_id" uuid NOT NULL, CONSTRAINT "PK_8402e5df5a30a229380e83e4f7e" PRIMARY KEY ("id"))`,
 		);
 		await queryRunner.query(
-			`CREATE TABLE "transactions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "amount" integer NOT NULL, "status" "public"."transactions_status_enum" NOT NULL, "source_wallet_id" uuid NOT NULL, "destination_wallet_id" uuid NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT '"2025-05-31T04:42:02.797Z"', "updated_at" TIMESTAMP NOT NULL DEFAULT '"2025-05-31T04:42:02.798Z"', CONSTRAINT "PK_a219afd8dd77ed80f5a862f1db9" PRIMARY KEY ("id"))`,
+			`CREATE TYPE "public"."transactions_type_enum" AS ENUM('TRANSFER', 'DEPOSIT')`,
+		);
+		await queryRunner.query(
+			`CREATE TYPE "public"."transactions_status_enum" AS ENUM('COMPLETED', 'REFUNDED')`,
+		);
+		await queryRunner.query(
+			`CREATE TABLE "transactions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "amount" integer NOT NULL, "type" "public"."transactions_type_enum" NOT NULL, "status" "public"."transactions_status_enum" NOT NULL, "source_wallet_id" uuid, "destination_wallet_id" uuid, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_a219afd8dd77ed80f5a862f1db9" PRIMARY KEY ("id"))`,
 		);
 		await queryRunner.query(
 			`ALTER TABLE "wallets" ADD CONSTRAINT "FK_92558c08091598f7a4439586cda" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -35,6 +41,8 @@ export class Migration1748666515557 implements MigrationInterface {
 			`ALTER TABLE "wallets" DROP CONSTRAINT "FK_92558c08091598f7a4439586cda"`,
 		);
 		await queryRunner.query(`DROP TABLE "transactions"`);
+		await queryRunner.query(`DROP TYPE "public"."transactions_status_enum"`);
+		await queryRunner.query(`DROP TYPE "public"."transactions_type_enum"`);
 		await queryRunner.query(`DROP TABLE "wallets"`);
 		await queryRunner.query(`DROP TABLE "users"`);
 	}
